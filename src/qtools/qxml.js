88097b4f00a74985ad1607ac59df2ddd393852b0
Bekir - CDI <b.burakcebecii@gmail.com>
2022-11-04 12:14:13 +0100

    installed xtml instead of xml2json..

--- src/qtools/qxml.js
+++ src/qtools/qxml.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import * as qsys from './qsys.js';
-import parser from 'xml2json';
+import parse from 'tmxl';
 
 /**
  * Convert XML file to JavaScript object.
@@ -19,7 +19,7 @@ export const getXmlFileAsObject = (pathAndFileName) => {
 	const fullPathAndFileName =
 		qsys.buildSystemAbsolutePathAndFileName(pathAndFileName);
 	const xmlAsText = fs.readFileSync(fullPathAndFileName, 'utf8');
-	const xmlAsJson = parser.toJson(xmlAsText);
+	const xmlAsJson = parse.toJson(xmlAsText);
 	const xmlAsObject = JSON.parse(xmlAsJson);
 	return xmlAsObject;
 };
