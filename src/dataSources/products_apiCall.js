5cac14cd6f06879701250a8f9e4321f359c928ca
Bekir - CDI <b.burakcebecii@gmail.com>
2022-11-04 16:11:06 +0100

    products api url changed. becasuse of hetzner..

--- src/dataSources/products_apiCall.js
+++ src/dataSources/products_apiCall.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const products = (
-	await axios.get('https://teknoza-backend.bscebeci.de/api/products')
-).data;
+const products = (await axios.get('http://localhost:3484/api/products')).data;
 
 export default products;
