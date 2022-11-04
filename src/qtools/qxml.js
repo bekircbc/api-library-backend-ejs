514b6c7808c6fc372365f94256d06b9f3d182148
Bekir - CDI <b.burakcebecii@gmail.com>
2022-11-04 13:11:10 +0100

    all console logs deleted..

--- src/qtools/qxml.js
+++ src/qtools/qxml.js
@@ -21,13 +21,9 @@ const options = { ignoreComment: true, alwaysChildren: true };
 export const getXmlFileAsObject = (pathAndFileName) => {
 	const fullPathAndFileName =
 		qsys.buildSystemAbsolutePathAndFileName(pathAndFileName);
-	console.log(fullPathAndFileName);
 	const xmlAsText = fs.readFileSync(fullPathAndFileName, 'utf8');
-	console.log(xmlAsText);
 	const xmlAsJson = pkg.xml2json(xmlAsText, options);
-	console.log(xmlAsJson);
 
 	const xmlAsObject = JSON.parse(xmlAsJson);
-	console.log(xmlAsObject);
 	return xmlAsObject;
 };
